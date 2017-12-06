c5d816f2eb3df9ab0e3c5d4e308159d61bc09a42
Guy Coop <GCoop@outlook.com>
2017-12-06 15:44:35 +0000

    updates to data handlin

--- Documents/Neural/Python Code/Data_Handling.py
+++ Documents/Neural/Python Code/Data_Handling.py
@@ -1,6 +1,8 @@
 # note, check if co_ordinates start at 0,0 or 1,1 and fix get_pixel_list method in generate_output_file
 # note, if bounding box overlap is possible, then add compression to pixel_list santization function
 import matplotlib.pylab as plt
+import os
+import numpy as np
 
 class data_handler:
     image_size = [400, 400]
@@ -27,6 +29,13 @@ class data_handler:
         "train",
         "tvmonitor"
     ]
+
+    def __init__(self, _data_dir):
+        self.data_dir = _data_dir
+        self.train_dir = _data_dir + "/train/"
+        self.test_dir = _data_dir + "/test/"
+        return
+
 
     @staticmethod
     def generate_output_file(bounding_boxes, filename):
@@ -76,11 +85,46 @@ class data_handler:
         return output_string
 
     @staticmethod
-    def get_bounding_boxes():
+    def get_bounding_boxes(label):
         # from an input pixel list file, generate a dictionary of bounding boxes
+        def pixel_list_to_bboxes(pixel_list):
+            pixel_loc_corners = []
+            bboxes = []
+            #start pixel location, current left pixel location, run length
+            if pixel_list[0][0] == 1 and pixel_list[0][1] == 0:
+                return bounding_boxes
+            pixel_loc_corners.append([pixel_list[0][0], pixel_list[0][0], pixel_list[0][1]])
+            for pair in pixel_list[1:]:
+                for ind, objs_found in enumerate(pixel_loc_corners):
+                    if objs_found[1] + data_handler.image_size[1] == pair[0]:
+                        # not a new object
+                        pixel_loc_corners[ind][1] = pair[0]
+                    else:
+                        # found a new object
+                        pixel_loc_corners.append([pair[0], pair[0], pair[1]])
+            # finished building pixel_loc_corners
+            for corner in pixel_loc_corners:
+                x1 = ((corner[0]) % data_handler.image_size[0]) + 1
+                y1 = int(corner[0] / data_handler.image_size[1])
+                x2 = ((corner[1] + corner[2] - 1) % data_handler.image_size[0]) + 1
+                y2 = int((corner[1] + corner[2]) / data_handler.image_size[1])
+
+                print(corner[1])
+                bboxes.append([[x1, y1],[x2, y2]])
+
+            return bboxes
+
+
+        pixel_list_dict = {}
+        for ind, line in enumerate(label.readlines()):
+            pixel_list_dict[data_handler.object_list[ind]] = map(int, (line.split(',')[1].split()))
+            pixel_list_dict[data_handler.object_list[ind]] = np.reshape(
+                pixel_list_dict[data_handler.object_list[ind]],
+                [len(pixel_list_dict[data_handler.object_list[ind]])/2, 2])
+
         bounding_boxes = {}
         for obj in data_handler.object_list:
-            bounding_boxes[obj] = []
+            bounding_boxes[obj] = pixel_list_to_bboxes(pixel_list_dict[obj])
 
         return bounding_boxes
 
@@ -99,7 +143,8 @@ class data_handler:
         plt.imread(file_name)
         output_array = np.reshape()
 
+
 # test for generate_output_file method
 # generates an empty bounding box dictionary then attempts to print the output file
-bboxes = data_handler.get_bounding_boxes()
-print(data_handler.generate_output_file(bboxes, "img_000.jpg"))
+bboxes = data_handler.get_bounding_boxes(open("/home/guy/Documents/Neural/Data/train/2009_002053.txt"))
+print(bboxes["cat"])
